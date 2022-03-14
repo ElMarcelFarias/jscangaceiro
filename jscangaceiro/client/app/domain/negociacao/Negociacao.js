61feff66b82c345a705c36fcf95b9483166100d8
Marcel Leite de Farias <marcel.leitefarias@gmail.com>
2022-03-14 17:29:54 -0300

    página 54

--- jscangaceiro/client/app/domain/negociacao/Negociacao.js
+++ jscangaceiro/client/app/domain/negociacao/Negociacao.js
@@ -1,7 +1,22 @@
 class	Negociacao	{
-    constructor() {
-        this.data = new Date();
-        this.quantidade = 1;
-        this.valor = 0.0;
+    constructor(data, quantidade, valor) {
+        this._data = data;
+        this._quantidade = quantidade;
+        this._valor = valor;
+    }
+
+    get volume() {
+        return this._quantidade * this._valor;
+    }
+
+    get data(){
+        return this._data;
+    }
+    get quantidade(){
+        return this._quantidade;
+    }
+
+    get valor(){
+        return this._valor;
     }
 }
