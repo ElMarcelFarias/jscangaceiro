710e4a628fc65bb5fd403c3e9ff6ac7cdfa929af
Marcel Leite de Farias <marcel.leitefarias@gmail.com>
2022-03-23 15:15:06 -0300

    new commit pag 110

---
+++ jscangaceiro/client/app/controllers/NegociacaoController.js
@@ -0,0 +1,30 @@
+
+class NegociacaoController {
+
+    constructor() {
+        
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this.inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
+    adiciona(event) {
+
+        event.preventDefault();
+
+        let data = new Date(...
+            this._inputData.value
+                .split('-')
+                .map((item, indice) => item - indice % 2)
+        );
+
+        let negociacao = new Negociacao(
+            data,
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
+
+        console.log(negociacao);
+    }
+}
