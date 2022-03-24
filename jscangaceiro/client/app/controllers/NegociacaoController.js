2761174f2cf13467b2fe7df34dfff23d57db32af
Marcel Leite de Farias <marcel.leitefarias@gmail.com>
2022-03-24 14:30:36 -0300

    new commit: 24/03/2022 página 128

--- jscangaceiro/client/app/controllers/NegociacaoController.js
+++ jscangaceiro/client/app/controllers/NegociacaoController.js
@@ -13,17 +13,17 @@ class NegociacaoController {
 
         event.preventDefault();
 
-        let converter = new DateConverter();
-
-        let data = converter.paraData(this._inputData.value)
-
         let negociacao = new Negociacao(
-            data,
+            DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
-        );
+        )
 
-        let diaMesAno = converter.paraTexto(negociacao.data);
+        let converter = new DateConverter();
+
+        
+
+        let diaMesAno = DateConverter.paraTexto(negociacao.data);
         console.log(diaMesAno);
     }
 }
--- jscangaceiro/client/app/ui/converters/DateConverter.js
+++ jscangaceiro/client/app/ui/converters/DateConverter.js
@@ -1,12 +1,17 @@
 class DataConverter {
     
-    paraTexto(data){
-        return	data.getDate()
-        +	'/'	+	(data.getMonth()	+	1)
-        +	'/'	+	data.getFullYear();
+    constructor() {
+        throw new Error('Esta classe não pode ser instanciada')
     }
 
-    paraData(texto){
+    static paraTexto(data){
+        return	`${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
+    }
+
+    static paraData(texto){
+        if(!/^\d{4}-\d{2}-\d{2}$/.test(texto)){
+            throw new Error('Deve estar	no formato aaaa-mm-dd')
+        }
         return new Date(...texto.split('-').map((item,indice) =>	
         item - indice %	2));
     }
