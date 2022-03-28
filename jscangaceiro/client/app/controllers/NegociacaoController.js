f804f7e6ee6e02bbeee7fbe5c5acfd6c3524a174
Marcel Leite de Farias <marcel.leitefarias@gmail.com>
2022-03-28 16:13:46 -0300

    new commit cap 6

--- jscangaceiro/client/app/controllers/NegociacaoController.js
+++ jscangaceiro/client/app/controllers/NegociacaoController.js
@@ -1,29 +1,36 @@
-
 class NegociacaoController {
-
     constructor() {
-        
+
         let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
-        this.inputQuantidade = $('#quantidade');
+        this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
     }
 
     adiciona(event) {
 
         event.preventDefault();
+        this._negociacoes.adiciona(this._criaNegociacao());
+        console.log(this._negociacoes);
+        this._limpaFormulario();
+        
+    }
 
-        let negociacao = new Negociacao(
+    _limpaFormulario() {
+
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
+        this._inputData.focus();
+    }
+
+    _criaNegociacao() {
+
+        return new Negociacao(
             DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value)
-        )
-
-        let converter = new DateConverter();
-
-        
-
-        let diaMesAno = DateConverter.paraTexto(negociacao.data);
-        console.log(diaMesAno);
+        );
     }
 }
--- jscangaceiro/client/app/domain/negociacao/Negociacoes.js
+++ jscangaceiro/client/app/domain/negociacao/Negociacoes.js
@@ -1,9 +1,17 @@
 class Negociacoes {
+
     constructor() {
+
         this._negociacoes = [];
     }
 
     adiciona(negociacao) {
+
         this._negociacoes.push(negociacao);
+    }
+
+    paraArray() {
+
+        return [].concat(this._negociacoes);
     }
 }
